24c1d0319a951f39edcb543d5e6e140920cf7a3f
victorvieiradev <contatovictorvieira@outlook.com>
2022-08-23 20:56:31 -0300

    projeto feito

---
+++ src/main/java/com/example/banco/exception/ExceptionHandlerContaBancaria.java
@@ -0,0 +1,47 @@
+package com.example.banco.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionHandlerContaBancaria extends ResponseEntityExceptionHandler{
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String mensagemUser = messageSource.getMessage("mensagem.invalida", null, null);
+        String mensagemDev = ex.getCause().toString();
+        return handleExceptionInternal(ex, new MensagemErro(mensagemUser, mensagemDev), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MensagemErro{
+
+        private String mensagemDoUsuario;
+        private String mensagemDoDev;
+
+    }
+
+
+}
+
